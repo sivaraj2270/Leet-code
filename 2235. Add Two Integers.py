ec3a3a453c9693fe5617719b136bf2d2e9828f19
M. SIVARAJ <msivaraj172@gmail.com>
2026-07-11 15:36:20 +0530

    Refactor docstring in Add Two Integers.py
    Updated docstring format and added example for clarity.

---
+++ 2235. Add Two Integers.py
@@ -0,0 +1,13 @@
+'''Add Two Integers
+
+Example 1:
+
+Input: num1 = 12, num2 = 5
+Output: 17
+Explanation: num1 is 12, num2 is 5, and their sum is 12 + 5 = 17, so 17 is returned.'''
+
+#code link : https://leetcode.com/problems/add-two-integers/description/
+
+class Solution:
+    def sum(self, num1: int, num2: int) -> int:
+        return (num1 + num2)
