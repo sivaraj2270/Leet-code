b3dbd76ea79659d2509c107f8760fe9af5d7d93c
M. SIVARAJ <msivaraj172@gmail.com>
2026-08-10 21:59:50 +0530

    Add palindrome number checking function
    Implement palindrome check for an integer.

---
+++ 9. Palindrome Number.py
@@ -0,0 +1,19 @@
+'''9. Palindrome Number
+
+Example 1:
+
+Input: x = 121
+Output: true
+Explanation: 121 reads as 121 from left to right and from right to left.'''
+
+#code link : https://leetcode.com/problems/palindrome-number/description/
+
+
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        a = str(x)
+        b = a[::-1]
+        if b == a:
+            return True
+        else:
+            return False
