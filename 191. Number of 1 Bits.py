5749b365c96451a1cb0a23a765fbfb88df232e7f
M. SIVARAJ <msivaraj172@gmail.com>
2026-08-20 07:02:53 +0530

    Add solution for counting number of 1 bits
    Implement Hamming weight calculation for an integer.

---
+++ 191. Number of 1 Bits.py
@@ -0,0 +1,22 @@
+'''191. Number of 1 Bits
+
+Example 1:
+
+Input: n = 11
+
+Output: 3
+
+Explanation:
+
+The input binary string 1011 has a total of three set bits.'''
+
+
+#code link : https://leetcode.com/problems/number-of-1-bits/description/
+
+class Solution:
+    def hammingWeight(self, n: int) -> int:
+        count = 0
+        a = bin(n)
+        b = a.count('1')
+        return b
+        
